f428cb6f45f24e58267e60f71bf2d50d4b9756c7
BerserkerLan <rusab3529>
2020-10-20 14:53:20 +0100

    Updates

---
+++ precache-manifest.b74d2a5cc32e0f11894984ac23297836.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8dd01a828653cb1cd0c50dea7c77fd",
+    "url": "/IslamNow-React/index.html"
+  },
+  {
+    "revision": "b90a03cb296107a9cf86",
+    "url": "/IslamNow-React/static/css/main.d65757ed.chunk.css"
+  },
+  {
+    "revision": "a9ecf644df9cae307e6d",
+    "url": "/IslamNow-React/static/js/2.ffff3682.chunk.js"
+  },
+  {
+    "revision": "8dbc9bd92b344a5deca62eeacc09fc2d",
+    "url": "/IslamNow-React/static/js/2.ffff3682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b90a03cb296107a9cf86",
+    "url": "/IslamNow-React/static/js/main.869b7ee4.chunk.js"
+  },
+  {
+    "revision": "ec782fe7ca5edaf6599c",
+    "url": "/IslamNow-React/static/js/runtime-main.7770d5fc.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/IslamNow-React/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/IslamNow-React/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/IslamNow-React/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/IslamNow-React/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "199e9ec14308832c6a8a49bd96c1fde6",
+    "url": "/IslamNow-React/static/media/bg4.199e9ec1.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/IslamNow-React/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/IslamNow-React/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/IslamNow-React/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/IslamNow-React/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "2e7cdeb4ad8c4ce2b38a388149ad36db",
+    "url": "/IslamNow-React/static/media/edinburgh-masjid.2e7cdeb4.jpg"
+  },
+  {
+    "revision": "7a2e7622362e74fc2bb93cabb8da32fb",
+    "url": "/IslamNow-React/static/media/email_icon.7a2e7622.svg"
+  },
+  {
+    "revision": "7be5b44bf3e313b496e3f1a2757e24a0",
+    "url": "/IslamNow-React/static/media/github_icon.7be5b44b.svg"
+  },
+  {
+    "revision": "733d06d1bce1af42592f397ca2f38c6f",
+    "url": "/IslamNow-React/static/media/islam_logo.733d06d1.png"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/IslamNow-React/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/IslamNow-React/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/IslamNow-React/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/IslamNow-React/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "b8b6ff95ada900b789de0f1cd0ebd035",
+    "url": "/IslamNow-React/static/media/person_on_computer_icon.b8b6ff95.svg"
+  },
+  {
+    "revision": "3efebc629e448cde3861e3f5f7b1ad27",
+    "url": "/IslamNow-React/static/media/praying_icon.3efebc62.svg"
+  },
+  {
+    "revision": "9f9272ad5cdf03b9a63481149a2a778f",
+    "url": "/IslamNow-React/static/media/process_icon.9f9272ad.svg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/IslamNow-React/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/IslamNow-React/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/IslamNow-React/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "/IslamNow-React/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/IslamNow-React/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/IslamNow-React/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/IslamNow-React/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/IslamNow-React/static/media/studio-5.ef5c30ea.jpg"
+  },
+  {
+    "revision": "7d8d2e4c9d90f919ebea01578cc98176",
+    "url": "/IslamNow-React/static/media/team1.7d8d2e4c.jpg"
+  },
+  {
+    "revision": "ddd13d62698e84c9d7ab7ea32d0751a9",
+    "url": "/IslamNow-React/static/media/team2.ddd13d62.jpg"
+  },
+  {
+    "revision": "d07a9f874f91a98cb2e78ca762c4428c",
+    "url": "/IslamNow-React/static/media/team3.d07a9f87.jpg"
+  },
+  {
+    "revision": "71d2309faa0261f24a3836ff0f5d627b",
+    "url": "/IslamNow-React/static/media/team4.71d2309f.jpg"
+  },
+  {
+    "revision": "cf334a9452b8f6e60bbc18b89990e3ac",
+    "url": "/IslamNow-React/static/media/website_icon.cf334a94.svg"
+  }
+]);
